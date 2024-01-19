4ecb03912d41e1a293038649b60847665336b45e
mayonnaize <mechgs78@yahoo.co.jp>
2024-01-19 21:48:05 +0900

    初期追加

---
+++ Source/UE_sandbox/UE_sandboxGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE_sandboxGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_SANDBOX_API AUE_sandboxGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
