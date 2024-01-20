5c0b7abf1da8b5ebf51243b9d8ce0ceedf8a6b2c
mayonnaize <mechgs78@yahoo.co.jp>
2024-01-20 16:25:33 +0900

    ビルドする度に.gradleを削除する

--- Source/UE_sandbox/UE_sandbox.Build.cs
+++ Source/UE_sandbox/UE_sandbox.Build.cs
@@ -1,5 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+using System;
+using System.IO;
 using UnrealBuildTool;
 
 public class UE_sandbox : ModuleRules
@@ -12,12 +14,29 @@ public class UE_sandbox : ModuleRules
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+		if (Target.Platform == UnrealTargetPlatform.Android)
+		{
+            deleteAndroidIntermediate("arm64");
+
+        }
+    }
+
+	private void deleteAndroidIntermediate(string arch)
+	{
+		string dirPath = Path.Combine(ModuleDirectory, "../../Intermediate/Android/", arch, "gradle/.gradle");
+
+		if (Directory.Exists(dirPath))
+		{
+			try
+			{
+				Directory.Delete(dirPath, true);
+				Console.WriteLine(dirPath + "is deleted");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("[ERROR]" + dirPath + "cannot be deleted: " + ex.ToString());
+			}
+		}
 	}
 }
